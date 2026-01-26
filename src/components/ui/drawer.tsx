e74d18b90283eda3d5ed8d1e32dbeef24d70b886
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2026-01-26 09:34:08 +0100

    fix cn path

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { Drawer as DrawerPrimitive } from "vaul"
 
-import { cn } from "@components/lib/utils"
+import { cn } from "@lib/util/utils"
 
 const Drawer = ({
   shouldScaleBackground = true,
